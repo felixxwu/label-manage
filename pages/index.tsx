447a0c2e292237ba118b14fe082cdfbd53a37fa2
Felix Wu <fwu@shipserv.com>
2022-09-29 18:05:31 +0100

    update favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,8 @@ export default function Home() {
                 <link rel='preconnect' href='https://fonts.googleapis.com' />
                 <link rel='preconnect' href='https://fonts.gstatic.com' />
                 <link href='https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap' rel='stylesheet' />
+                <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
+                <link rel='shortcut icon' href='/favicon.svg' />
             </Head>
 
             <main>
