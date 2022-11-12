eac838ea699c91d5869624486124a92671eebeeb
Felix Wu <fwu@shipserv.com>
2022-11-12 17:10:57 +0000

    don't suggest submitted songs

--- components/forms/SongsSubmittedForm.tsx
+++ components/forms/SongsSubmittedForm.tsx
@@ -2,6 +2,7 @@ import { store } from '../../utils/store'
 import { Label } from '../../utils/types'
 import { updateDocTyped } from '../../utils/db'
 import { Chips } from '../Chips'
+import styled from '@emotion/styled'
 
 export function SongsSubmittedForm(props: { label: Label }) {
     async function handleDelete(song: string) {
@@ -10,7 +11,9 @@ export function SongsSubmittedForm(props: { label: Label }) {
         })
     }
 
-    const sortedSongs = store().extra.songs.sort((a, b) => (a.title > b.title ? 1 : -1))
+    const sortedSongs = store()
+        .extra.songs.filter(song => !props.label.songsSubmitted.includes(song.title))
+        .sort((a, b) => (a.title > b.title ? 1 : -1))
 
     return (
         <>
@@ -30,37 +33,38 @@ export function SongsSubmittedForm(props: { label: Label }) {
                     }
 
                     return (
-                        <div className='songs'>
+                        <Wrapper>
                             {sortedSongs.map(song => (
-                                <div onClick={() => handleSelection(song.title)} className='song' key={song.title}>
+                                <Song onClick={() => handleSelection(song.title)} key={song.title}>
                                     {song.title}
-                                </div>
+                                </Song>
                             ))}
-                            {sortedSongs.length === 0 && <div className='error'>No songs to choose from</div>}
-                        </div>
+                            {sortedSongs.length === 0 && (
+                                <Error className='error'>No songs to choose from</Error>
+                            )}
+                        </Wrapper>
                     )
                 }}
             />
-            <style jsx>{`
-                .songs {
-                    display: flex;
-                    flex-direction: column;
-                }
-
-                .song {
-                    padding: 20px;
-                    min-width: 300px;
-                    cursor: pointer;
-                }
-
-                .song:hover {
-                    background-color: rgba(0, 0, 0, 0.05);
-                }
-
-                .error {
-                    padding: 20px;
-                }
-            `}</style>
         </>
     )
 }
+
+const Wrapper = styled('div')`
+    display: flex;
+    flex-direction: column;
+`
+
+const Song = styled('div')`
+    padding: 20px;
+    min-width: 300px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.05);
+    }
+`
+
+const Error = styled('div')`
+    padding: 20px;
+`
