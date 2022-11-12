53a5e5f54e542d7d870052e2a9a23250bbf02d80
Felix Wu <fwu@shipserv.com>
2022-11-12 17:07:13 +0000

    allow clearing follower form

--- components/forms/FollowersForm.tsx
+++ components/forms/FollowersForm.tsx
@@ -1,20 +1,22 @@
+import styled from '@emotion/styled'
 import { ToggleButton, ToggleButtonGroup } from '@mui/material'
 import React, { useState } from 'react'
 import { updateDocTyped } from '../../utils/db'
 import { store } from '../../utils/store'
 import { followerOptions, Label } from '../../utils/types'
+import { ClearButton } from '../ClearButton'
 import { EditButton } from '../EditButton'
 
 export function FollowersForm(props: { label: Label }) {
     const [followers, setFollowers] = useState(props.label.followers)
     const [editMode, setEditMode] = useState(false)
 
-    function handleChange(_, newFollowers: typeof followerOptions[number]) {
+    function handleChange(newFollowers: typeof followerOptions[number]) {
+        updateDocTyped(store().db, props.label.id, { followers: newFollowers })
+        setFollowers(newFollowers)
         if (newFollowers) {
-            updateDocTyped(store().db, props.label.id, { followers: newFollowers })
-            setFollowers(newFollowers)
+            toggleEditMode()
         }
-        toggleEditMode()
     }
 
     function toggleEditMode() {
@@ -22,9 +24,14 @@ export function FollowersForm(props: { label: Label }) {
     }
 
     return (
-        <div className='form'>
+        <Wrapper>
             {editMode ? (
-                <ToggleButtonGroup color='primary' exclusive value={followers} onChange={handleChange}>
+                <ToggleButtonGroup
+                    color='primary'
+                    exclusive
+                    value={followers}
+                    onChange={(_, value) => handleChange(value)}
+                >
                     {followerOptions.map((option, index) => (
                         <ToggleButton value={option} key={index}>
                             {option}
@@ -32,28 +39,37 @@ export function FollowersForm(props: { label: Label }) {
                     ))}
                 </ToggleButtonGroup>
             ) : (
-                <div className='count'>
+                <Count>
                     {props.label.followers ? (
-                        `~ ${props.label.followers} followers`
+                        <>
+                            ~ ${props.label.followers} followers
+                            <ClearButton
+                                onClick={() => handleChange(null)}
+                                secondary={!!props.label.followers}
+                            />
+                        </>
                     ) : (
-                        <div style={{ opacity: 0.5 }}>Followers:</div>
+                        <>
+                            <div style={{ opacity: 0.5 }}>Followers:</div>
+                            <EditButton
+                                onClick={toggleEditMode}
+                                secondary={!!props.label.followers}
+                            />
+                        </>
                     )}
-                    <EditButton onClick={toggleEditMode} secondary={!!props.label.followers} />
-                </div>
+                </Count>
             )}
-
-            <style jsx>{`
-                .form {
-                    width: 100%;
-                }
-
-                .count {
-                    display: flex;
-                    align-items: center;
-                    gap: 20px;
-                    justify-content: space-between;
-                }
-            `}</style>
-        </div>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled('div')`
+    width: 100%;
+`
+
+const Count = styled('div')`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    justify-content: space-between;
+`
