958aab9e2d9e7254a4d1596031f858be02678748
Felix Wu <fwu@shipserv.com>
2023-03-17 15:47:56 +0000

    update list item icons

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -9,121 +9,113 @@ import { getProgress } from '../utils/progress'
 import styled from '@emotion/styled'
 import { Chips } from './Chips'
 import QuestionMarkIcon from '@mui/icons-material/HelpOutline'
-import MusicNoteIcon from '@mui/icons-material/MusicNote'
 import { areAllSongsDealtWith } from '../utils/allSongsDealtWith'
 import { nFormatter } from '../utils/nFormatter'
 import HourglassDisabledIcon from '@mui/icons-material/HourglassDisabled'
 import { getDaysAgo } from '../utils/getDaysAgo'
+import DoneIcon from '@mui/icons-material/Done'
 
 export function ListItem(props: { label: Label; index: number }) {
-    async function handleClick() {
-        window.location.href += '/label/' + props.label.id
-    }
+  async function handleClick() {
+    window.location.href += '/label/' + props.label.id
+  }
 
-    return (
-        <>
-            <Wrapper
-                onClick={handleClick}
-                compact={store().extra.compact}
-                index={props.index}
-                inactive={props.label.inactive}
-            >
-                <Header>
-                    <Name>
-                        {!store().extra.compact && (
-                            <Avatar
-                                src={props.label.image}
-                                sx={{
-                                    width: consts.listAvatarSize,
-                                    height: consts.listAvatarSize,
-                                }}
-                            />
-                        )}
+  return (
+    <>
+      <Wrapper
+        onClick={handleClick}
+        compact={store().extra.compact}
+        index={props.index}
+        inactive={props.label.inactive}
+      >
+        <Header>
+          <Name>
+            {!store().extra.compact && (
+              <Avatar
+                src={props.label.image}
+                sx={{
+                  width: consts.listAvatarSize,
+                  height: consts.listAvatarSize,
+                }}
+              />
+            )}
 
-                        {props.label.name}
+            {props.label.name}
 
-                        {props.label.submission &&
-                            (props.label.submission.includes('@') ? (
-                                <EmailIcon
-                                    fontSize={store().extra.compact ? 'small' : 'medium'}
-                                    opacity={0.5}
-                                />
-                            ) : (
-                                <LinkIcon
-                                    fontSize={store().extra.compact ? 'small' : 'medium'}
-                                    opacity={0.5}
-                                />
-                            ))}
-                    </Name>
-                    <Icons>
-                        {areAllSongsDealtWith(props.label) && (
-                            <MusicNoteIcon color='primary' fontSize='small' />
-                        )}
-                        {getDaysAgo(props.label) > consts.uploadWarning && (
-                            <HourglassDisabledIcon
-                                fontSize={store().extra.compact ? 'small' : 'medium'}
-                            />
-                        )}
-                        {getProgress(props.label) !== 100 && (
-                            <QuestionMarkIcon
-                                fontSize={store().extra.compact ? 'small' : 'medium'}
-                            />
-                        )}
-                        <Followers>{nFormatter(props.label.followers, 0)}</Followers>
-                    </Icons>
-                </Header>
-                {props.label.styles.length !== 0 && !store().extra.compact && (
-                    <div>
-                        <Chips chips={props.label.styles} small colorful />
-                    </div>
+            {props.label.submission &&
+              (props.label.submission.includes('@') ? (
+                <EmailIcon fontSize={store().extra.compact ? 'small' : 'medium'} opacity={0.5} />
+              ) : (
+                <LinkIcon fontSize={store().extra.compact ? 'small' : 'medium'} opacity={0.5} />
+              ))}
+          </Name>
+          <Icons>
+            {!props.label.inactive && (
+              <>
+                {getDaysAgo(props.label) > consts.uploadWarning && (
+                  <HourglassDisabledIcon fontSize={store().extra.compact ? 'small' : 'medium'} />
                 )}
-            </Wrapper>
-        </>
-    )
+                {getProgress(props.label) !== 100 && (
+                  <QuestionMarkIcon fontSize={store().extra.compact ? 'small' : 'medium'} />
+                )}
+                {areAllSongsDealtWith(props.label) && <DoneIcon color='primary' fontSize='small' />}
+              </>
+            )}
+            <Followers>{nFormatter(props.label.followers, 0)}</Followers>
+          </Icons>
+        </Header>
+        {props.label.styles.length !== 0 && !store().extra.compact && (
+          <div>
+            <Chips chips={props.label.styles} small colorful />
+          </div>
+        )}
+      </Wrapper>
+    </>
+  )
 }
 
 const Wrapper = styled('div')<{ compact: boolean; index: number; inactive: boolean }>`
-    width: 100%;
-    max-width: ${consts.maxAppWidth}px;
-    color: ${theme.palette.primary.main};
-    background-color: ${({ compact, index }) =>
-        compact && index % 2 === 0 ? '' : theme.palette.secondary.main};
-    padding: ${({ compact }) => (compact ? '2px' : '20px')};
-    border-radius: ${({ compact }) => (compact ? 0 : consts.borderRadius)}px;
-    cursor: pointer;
-    text-align: left;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    opacity: ${({ inactive }) => (inactive ? 0.5 : 1)};
-    transition: 300ms;
+  width: 100%;
+  max-width: ${consts.maxAppWidth}px;
+  color: ${theme.palette.primary.main};
+  background-color: ${({ compact, index }) =>
+    compact && index % 2 === 0 ? '' : theme.palette.secondary.main};
+  padding: ${({ compact }) => (compact ? '2px' : '20px')};
+  border-radius: ${({ compact }) => (compact ? 0 : consts.borderRadius)}px;
+  cursor: pointer;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  opacity: ${({ inactive }) => (inactive ? 0.5 : 1)};
+  transition: 300ms;
 
-    &:hover {
-        background-color: ${({ compact }) => (compact ? '' : theme.palette.secondary.light)};
-    }
+  &:hover {
+    background-color: ${({ compact }) => (compact ? '' : theme.palette.secondary.light)};
+  }
 `
 
 const Header = styled('div')`
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    gap: 10px;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  gap: 10px;
 `
 
 const Name = styled('div')`
-    display: flex;
-    align-items: center;
-    gap: 10px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
 `
 
 const Icons = styled('div')`
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    opacity: 0.5;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  opacity: 0.5;
 `
 
 const Followers = styled('div')`
-    min-width: 36px;
-    text-align: right;
+  min-width: 36px;
+  text-align: right;
 `
